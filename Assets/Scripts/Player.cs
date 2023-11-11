b3c0ebb494ecc70865ef13313460e74463e9c9ed
PolGamesInc <company.polgames@mail.ru>
2023-11-11 16:49:49 +0600

    second commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,10 +6,12 @@ public class Player : MonoBehaviour
 {
     private float speed = 20;
     [SerializeField] private GameObject ObstaclePrefab;
+    [SerializeField] private GameObject[] Obstacle;
 
     private void Start()
     {
         SpawnObstacle();
+        Obstacle = GameObject.FindGameObjectsWithTag("Obstacle");
     }
 
     private void Update()
@@ -19,7 +21,10 @@ public class Player : MonoBehaviour
 
     private void SpawnObstacle()
     {
-        Instantiate(ObstaclePrefab, new Vector3(Random.Range(-45, 45), 1.5f, Random.Range(-45, 45)), Quaternion.Euler(0, 0, 0));
+        for (int i = 0; i < 15; i++)
+        {
+            Instantiate(ObstaclePrefab, new Vector3(Random.Range(-45, 45), 1.5f, Random.Range(-45, 45)), Quaternion.Euler(0, 0, 0));
+        }
     }
 
     private void Move()
@@ -30,4 +35,22 @@ public class Player : MonoBehaviour
         float v = Input.GetAxis("Vertical");
         transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * speed * v);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject obstacle = other.gameObject;
+
+        if (other.tag == "Obstacle")
+        {
+            for (int i = 0; i < Obstacle.Length; i++)
+            {
+                if(obstacle == Obstacle[i])
+                {
+                    Destroy(obstacle);
+                    break;
+                }
+            }
+        }
+    }
+
 }
